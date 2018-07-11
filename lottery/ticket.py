10cabbf0f7a7cdaad2a34f37af863c4c99a9f643
flychensc <flychensc@163.com>
2018-07-11 23:24:46 +0800

    七星彩测试code

--- lottery/ticket.py
+++ lottery/ticket.py
@@ -14,3 +14,7 @@ payload = {'name': 'ssq', "issueCount":'30'}
 r = requests.get(url=url, params=payload, headers=headers)
 
 print(r.json())
+
+url = 'http://www.lottery.gov.cn/historykj/history.jspx?page=false&_ltype=qxc&termNum=50&startTerm=&endTerm='
+r = requests.get(url=url)
+print(r.text)
